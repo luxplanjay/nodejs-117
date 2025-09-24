60809d243f7fd75ac7a3f94773712f80a64258be
Alexander Repeta <alexander.repeta@gmail.com>
2025-09-24 20:30:43 +0300

    Add basic endpoits

--- src/server.js
+++ src/server.js
@@ -1,7 +1,30 @@
 import express from "express";
+import cors from "cors";
+import pino from "pino-http";
+import helmet from "helmet";
+import "dotenv/config";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(pino());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`Time : ${new Date().toISOString()}`);
+  next();
+});
+
+app.get("/error", () => {
+  throw new Error("This is a forced error.");
+});
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -9,12 +32,35 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/todos", (req, res) => {
-  res.status(200).json(["todo 1", "todo 2", "todo 3"]);
+app.post("/students", (req, res) => {
+  console.log(req.body);
+  res.status(201).json({ message: "POST request received" });
 });
 
-app.post("/students", (req, res) => {
-  res.status(201).json({ message: "New student" });
+app.get("/students", (req, res) => {
+  res.status(200).json([]);
+});
+
+app.get("/students/:studentId", (req, res) => {
+  const { studentId } = req.params;
+  console.log("Student ID:", studentId);
+  res.status(200).json({
+    message: `Details of student with ID: ${studentId}`,
+  });
+});
+
+// 404 middleware
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.log("Error Middleware:", err.message);
+  res.status(500).json({
+    error: err.message,
+  });
 });
 
 app.listen(PORT, () => {
